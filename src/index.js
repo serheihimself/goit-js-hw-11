a209ab9167924e26a0b472373a0c6efdf83fd1af
serheihimself <sergm080@gmail.com>
2023-01-19 16:34:10 +0200

    add dom & librares commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { fetchImages } from "./fetchImages";
+import Notiflix from "notiflix";
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const input = document.querySelector('.search-form-input');
+const btnSearch = document.querySelector('.search-form-button');
+const gallery = document.querySelector('.gallery');
+const btnLoadMore = document.querySelector('.load-more');
+let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
