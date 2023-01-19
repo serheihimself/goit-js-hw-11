42a8c0936c0a0029ef51315a40ebd11090a93797
serheihimself <sergm080@gmail.com>
2023-01-19 16:43:45 +0200

    create dom commit

--- src/index.js
+++ src/index.js
@@ -35,4 +35,48 @@ btnSearch.addEventListener('click', e => {
     }
   });
   
+  btnLoadMore.addEventListener('click', () => {
+    pageNumber++;
+    const trimmedValue = input.value.trim();
+    btnLoadMore.style.display = 'none';
+    fetchImages(trimmedValue, pageNumber).then(foundData => {
+      if (foundData.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderImageList(foundData.hits);
+        Notiflix.Notify.success(
+          `Hooray! We found ${foundData.totalHits} images.`
+        );
+        btnLoadMore.style.display = 'block';
+      }
+    });
+  });
   
+  function renderImageList(images) {
+    console.log(images, 'images');
+    const markup = images
+      .map(image => {
+        console.log('img', image);
+        return `<div class="photo-card">
+         <a href="${image.largeImageURL}"><img class="photo" src="${image.webformatURL}" alt="${image.tags}" title="${image.tags}" loading="lazy"/></a>
+          <div class="info">
+             <p class="info-item">
+      <b>Likes</b> <span class="info-item-api"> ${image.likes} </span>
+  </p>
+              <p class="info-item">
+                  <b>Views</b> <span class="info-item-api">${image.views}</span>  
+              </p>
+              <p class="info-item">
+                  <b>Comments</b> <span class="info-item-api">${image.comments}</span>  
+              </p>
+              <p class="info-item">
+                  <b>Downloads</b> <span class="info-item-api">${image.downloads}</span> 
+              </p>
+          </div>
+      </div>`;
+      })
+      .join('');
+    gallery.innerHTML += markup;
+  }
