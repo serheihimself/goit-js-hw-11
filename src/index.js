007d61f26a6918fc64b2b3d07f8268bbb5c569be
serheihimself <sergm080@gmail.com>
2023-01-19 16:38:56 +0200

    main addEvList commit

--- src/index.js
+++ src/index.js
@@ -9,3 +9,30 @@ const gallery = document.querySelector('.gallery');
 const btnLoadMore = document.querySelector('.load-more');
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
+btnLoadMore.style.display = 'none';
+
+let pageNumber = 1;
+
+btnSearch.addEventListener('click', e => {
+    e.preventDefault();
+    cleanGallery();
+    const trimmedValue = input.value.trim();
+    if (trimmedValue !== '') {
+      fetchImages(trimmedValue, pageNumber).then(foundData => {
+        if (foundData.hits.length === 0) {
+          Notiflix.Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          renderImageList(foundData.hits);
+          Notiflix.Notify.success(
+            `Hooray! We found ${foundData.totalHits} images.`
+          );
+          btnLoadMore.style.display = 'block';
+          gallerySimpleLightbox.refresh();
+        }
+      });
+    }
+  });
+  
+  
